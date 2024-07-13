d11d52a2e23b569858e9f7ef315e7fd3dd509ac1
jatin <jatinwatts8@gmail.com>
2024-07-13 16:47:21 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,171 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize learning new skills, fostering a best developer ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I always try to implement the technologies exist worldwide",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Banking system with nextJs",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "WeaveChat",
+    des: "A modern web application similar to threads built with React and Next.js, offering real-time conversations and seamless social networking features.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg", "/ts.svg"],
+    link: "https://threads-clone-q2vaavsbt-jatinwatts8gmailcoms-projects.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Health Navigator",
+    des: "A cutting-edge healthcare web application built with Next.js and Appwrite, offering seamless patient management, real-time data synchronization, and secure medical record.",
+    img: "/p2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+    link: "https://healthnavigator.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Interactorium",
+    des: "A social media platforms where people can make communities on the basis of different categories and share content with each other category wise.",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/c.svg", "/next.svg"],
+    link: "https://interactorium.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "World Book",
+    des: "A social media platforms where people can make communities on the basis of different categories and share content with each other category wise.",
+    img: "/p4.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/re.svg"],
+    link: "https://world-book.vercel.app",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Jatin is very good in web development. When it comes to work, he is very serious and hardworking.",
+    name: "Paras Sharma",
+    title: "Software Engineer at Mindsprint",
+  },
+  {
+    quote:
+      "Jatin is an awesome developer! and, he is very hardworking and creative.",
+    name: "Vimalendu Dubey",
+    title: "Software  Engineer at Verizon",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/jatinwa",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "/",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/jatin-watts-69aaa820a/",
+  },
+];
